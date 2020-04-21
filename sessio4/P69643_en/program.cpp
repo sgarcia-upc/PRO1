7c0f282b06b0ec43d1325cbec613b916e1adc751
Your Name <you@example.com>
2020-04-21 02:22:22 +0200

    P69643 done is a string balanced ()[]

--- sessio4/P69643_en/program.cpp
+++ sessio4/P69643_en/program.cpp
@@ -8,43 +8,56 @@ using namespace std;
 
 bool checkString(string s)
 // Pre: cert
-// Post: retorna true si la string donada es una sequencia correcta de paretesis o claus, false en cas contrari
+// Post: retorna true si la string donada es una sequencia correcta de paretesis o corchetes, false en cas contrari
 {
-    bool cor = true;
-    for (int x=0; x < (int) s.size(); x++){
-        for (int y=0; y < (int) s.size(); y++){
-             if (x == '[')
-            {
-            cor = false;
-                cout << "e";
+    stack <char> st;
+
+    for (int i=0; i < int( s.size()); ++i) {
+        char x = s[i];
+        if (x == '(' or x == '[' ){
+            st.push(x);
+        } else {
+
+            if (not st.empty()){
+                if (x == ']') 
+                    if (st.top() != '[') {
+                        return false;
+                    }
+    
+                if (x == ')') 
+                    if ( st.top() != '(') {
+                        return false;
+                    }
+        
+                st.pop();
+            } else {
+                return false;
             }
         }
+            
     }
-    return cor;
+    
+    if (st.empty())
+        return true;
+    return false;
 }
 
 
 int main() {
     string s;
-    stack <string> stringStack;
-    while (cin>>s) {
-        stringStack.push(s);
+    while (getline(cin, s)) {
+        istringstream ss(s);
+        string x;
+        while (ss >> x) {
+            cout << x;
+            bool correcte = checkString(x);
+            if (correcte == true)
+                cout << " is correct";
+            else
+                cout << " is incorrect";
+
+            cout << endl;
+        }
     }
 
-
-    while (not stringStack.empty()){
-        cout << stringStack.top();
-        bool correcte = true;
-
-
-        if (correcte == true)
-            cout << " is correct";
-        else
-            cout << " is incorrect";
-
-        cout << endl;
-
-        stringStack.pop();
-
-    }
 }
